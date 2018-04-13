f80c7f2f0eaf83c1b571b9776cb57c0f844ba076
mjzimmermann <mjzimmermann@outlook.com>
2018-04-13 16:45:58 -0400

    Add test cases for GET /todos:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,10 +3,13 @@ const request = require('supertest');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {ObjectId} = require('mongodb');
 
 const todos = [{
+  _id: new ObjectId(),
   text: 'First test todo'
 }, {
+  _id: new ObjectId(),
   text: 'Second test todo'
 }]
 
@@ -69,4 +72,31 @@ describe('GET /todos', () => {
       })
       .end(done);
   })
-})
+});
+
+describe('GET /todos:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return a 404 if todo not found', (done) => {
+    var wrongID = new ObjectId().toHexString();
+    request(app)
+      .get(`/todos/${wrongID}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return a 404 for non-object IDs', (done) => {
+    request(app)
+      .get('/todos/123')
+      .expect(404)
+      .end(done);
+  });
+});
