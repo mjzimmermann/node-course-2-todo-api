891459e9cde84fc38bd37daae2a93d2062f082f3
mjzimmermann <mjzimmermann@outlook.com>
2018-05-16 10:39:55 -0400

    add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -68,6 +68,24 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user){
+      return Promise.reject('not found');
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (err || !res){
+          reject();
+        }
+        resolve(user);
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function(next) {
   var user = this;
   if (user.isModified('password')) {
